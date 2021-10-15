593b36153ababb2d63290f1f99806eb11e3382fc
Avneeth Kaur <90620251+xmain12@users.noreply.github.com>
2021-10-16 05:19:38 +0530

    Stefanie V 5.2 beta release✅

---
+++ plugins/sql/plugin.js
@@ -0,0 +1,32 @@
+/* Copyright (C) 2021 ws virus Fucker.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+VIRUS - FUCKER
+*/
+
+const config = require('../../config');
+const { DataTypes } = require('sequelize');
+
+const PluginDB = config.DATABASE.define('Plugin', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    url: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    }
+});
+
+async function installPlugin(adres, file) {
+    var Plugin = await PluginDB.findAll({
+        where: {url: adres}
+    });
+
+    if (Plugin.length >= 1) {
+        return false;
+    } else {
+        return await PluginDB.create({ url: adres, name: file });
+    }
+}
+module.exports = { PluginDB: PluginDB, installPlugin: installPlugin };
