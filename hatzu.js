55d411b5c72c13f9a8b7dec6a296fd1565ac3c83
Avneeth Kaur <90620251+xmain12@users.noreply.github.com>
2021-10-15 20:02:10 +0530

    Update hatzu.js

--- hatzu.js
+++ hatzu.js
@@ -1,7 +1,7 @@
-/* Copyright (C) 2021 TENUX-Neotro.
+/* Copyright (C) 2021 Stefanie-Vurus fucker.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-NEOTROX - TEENUHX
+STEFANIE - XMAIN
 */
 
 const { Sequelize } = require('sequelize');
