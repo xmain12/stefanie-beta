b67d38418156ef33efe88481db91a0d3a2b895b1
Avneeth Kaur <90620251+xmain12@users.noreply.github.com>
2021-10-16 07:08:08 +0530

    Stefanie V 5.2 beta release✅

--- plugins/admin.js
+++ plugins/admin.js
@@ -1551,7 +1551,7 @@ Asena.addCommand({pattern: 'unmute ?(.*)', fromMe: true, dontAddCommandList: tru
     }
 }));
 
-Asena.addCommand({pattern: 'invite ?(.*)', fromMe: true, dontAddCommandList: true, onlyGroup: true, desc: Lang.INVITE_DESC}, (async (message, match) => {    
+Asena.addCommand({pattern: 'linkgc ?(.*)', fromMe: true, dontAddCommandList: true, onlyGroup: true, desc: Lang.INVITE_DESC}, (async (message, match) => {    
     var im = await checkImAdmin(message);
     if (!im) return await message.client.sendMessage(message.jid,Lang.IM_NOT_ADMIN, MessageType.text);
     var invite = await message.client.groupInviteCode(message.jid);
