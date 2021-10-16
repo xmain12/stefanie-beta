fe5f2d6f01e715ec81abb51990fe865fed51fa30
Avneeth Kaur <90620251+xmain12@users.noreply.github.com>
2021-10-16 07:36:16 +0530

    Stefanie V 5.2 beta release✅

--- plugins/Setvarsl.js
+++ plugins/Setvarsl.js
@@ -1,4 +1,4 @@
-/* Copyright (C) 2021 ws virus Fucker.
+/* Copyright (C) 2021 ws virus-Fucker.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 VIRUS - FUCKER
