8235351dcae872e0a1a575305608d0d6b84e69a9
Avneeth Kaur <90620251+xmain12@users.noreply.github.com>
2021-10-16 09:42:17 +0530

    Stefanie V 5.2 beta release✅

--- helpers.js
+++ helpers.js
@@ -1,17 +1,17 @@
-/* Copyright (C) 2021 TENUX-Neotro.
+/* Copyright (C) 2021 ws virus Fucker.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-NEOTROX - TEENUHX
+VIRUS - FUCKER
 */
 
 function successfullMessage(msg) {
-    return "👩‍🦰 *AMAZONE ALEXA*:  ```" + msg + "```"
+    return "👩‍🦰 *QUEEN STEFANIE*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🚀 *AMAZONE ALEXA*:  ```" + msg + "```"
+    return "🚀 *QUEEN STEFANIE*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "🤖 *AMAZONE ALEXA*:  ```" + msg + "```"
+    return "🤖 *QUEEN STEFANIE*:  ```" + msg + "```"
 }
 
 
