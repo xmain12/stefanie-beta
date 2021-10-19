9d8f77664be036faf87ae6e587b1fb946aec55e7
@VirusFucker <90620251+xmain12@users.noreply.github.com>
2021-10-19 06:46:18 +0530

    Stefanie V 5.2 beta release✅

---
+++ plugins/tagmin.js
@@ -0,0 +1,56 @@
+/* Copyright (C) 2021 ws virus Fucker.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+VIRUS - FUCKER
+*/
+
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const Config = require('../config');
+
+const Language = require('../language');
+const Lang = Language.getString('tagall');
+
+if (Config.WORKTYPE == 'private') {
+    Asena.addCommand({pattern: 'tagmin$', fromMe: true, desc: Lang.TAGADMİN}, (async (message, match) => {
+        let grup = await message.client.groupMetadata(message.jid);
+        var jids = [];
+        mesaj = '';
+        grup['participants'].map(async (uye) => {
+            if (uye.isAdmin) {
+                mesaj += '👮‍♂️@' + uye.id.split('@')[0] + '\n';
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+            }
+        });
+        await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+    }));
+}
+else if (Config.WORKTYPE == 'public') {
+    Asena.addCommand({pattern: 'tagmin$', fromMe: false, desc: Lang.TAGADMİN}, (async (message, match) => {
+        let grup = await message.client.groupMetadata(message.jid);
+        var jids = [];
+        mesaj = '';
+        grup['participants'].map(async (uye) => {
+            if (uye.isAdmin) {
+                mesaj += '👮‍♂️@' + uye.id.split('@')[0] + '\n';
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+            }
+        });
+        await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+    }));
+}
+
+else if (Config.WORKTYPE == 'admin') {
+    Asena.addCommand({pattern: 'tagmin$', fromMe: false, desc: Lang.TAGADMİN}, (async (message, match) => {
+        let grup = await message.client.groupMetadata(message.jid);
+        var jids = [];
+        mesaj = '';
+        grup['participants'].map(async (uye) => {
+            if (uye.isAdmin) {
+                mesaj += '👮‍♂️@' + uye.id.split('@')[0] + '\n';
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+            }
+        });
+        await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {sendEphemeral: 'chat',contextInfo: {mentionedJid: jids}, previewType: 0})
+    }));
+}
